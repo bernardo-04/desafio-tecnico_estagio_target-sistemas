cdddd9737a76e61d723e61888ded74fa8ac64a81
Bernardo <bernardo.bcruz@gmail.com>
2024-10-24 21:27:10 -0300

    add the resolution of the question 03

--- searchA.js
+++ searchA.js
@@ -1,5 +1,5 @@
 /*
-  Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
+  2) Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
  */
 
 const checkChar = (string, char) => {
